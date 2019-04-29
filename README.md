563c2fba0fee3cb778acd8dd5260abebad4e3715
root <nikakw@gmail.com>
2019-04-28 21:13:25 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo contains different tools to be used for various InfoSec purposes. Currently all tools are written in Python.
---
+++ pythonports.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import socket
+ip = raw_input("Enter the ip: ")
+port = input("Enter the port: ")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+if s.connect_ex((ip, port)):
+        print "Port", port, "is closed."
+else:
+        print "Port", port, "is open."
